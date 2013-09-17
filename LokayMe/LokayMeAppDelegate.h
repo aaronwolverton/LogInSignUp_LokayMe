1fb7fb89c920da2b06d1c2fd982074c709effa58
Aaron Wolverton <aaron.wolverton@gmail.com>
2013-09-17 14:48:06 -0400

    Initial Commit

---
+++ LokayMe/LokayMeAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LokayMeAppDelegate.h
+//  LokayMe
+//
+//  Created by Aaron Wolverton on 9/17/13.
+//  Copyright (c) 2013 Aaron Wolverton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LokayMeAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LokayMe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LokayMe
+//
+//  Created by Aaron Wolverton on 9/17/13.
+//  Copyright (c) 2013 Aaron Wolverton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LokayMeAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LokayMeAppDelegate class]));
+    }
+}
